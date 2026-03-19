2f8b06f427db54068927b747340244ef9be00638
Jeong-Ryeol <jwr5336@naver.com>
2026-03-20 00:03:07 +0900

    [FEAT] Swagger OpenAPI 설정 구현

--- src/main/java/com/mumuk/global/config/SwaggerConfig.java
+++ src/main/java/com/mumuk/global/config/SwaggerConfig.java
@@ -1,5 +1,39 @@
 package com.mumuk.global.config;
 
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.servers.Server;
+
+
+@Configuration
 public class SwaggerConfig {
-    // TODO: Swagger/OpenAPI 설정
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes("BearerAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                                .in(SecurityScheme.In.HEADER)
+                                .name("Authorization"))) // Swagger UI에서 자동 주입
+                .addSecurityItem(new SecurityRequirement().addList("BearerAuth"))
+                .addServersItem(new Server().url("/"))
+                .info(apiInfo());
+    }
+
+
+    private Info apiInfo() {
+        return new Info()
+                .title("오늘 뭐 해먹지? Swagger")
+                .description("오늘 뭐 해먹지 팀의 Swagger 입니다.")
+                .version("1.0");
+    }
 }
