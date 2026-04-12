e79c796792e24e7cd848e34f982c6f6e4af738b5
jaemin0413 <95339052+jaemin0413@users.noreply.github.com>
2026-04-13 00:57:39 +0900

    [FEAT] jwt 관련 설정 추가

--- src/main/java/com/mumuk/global/security/jwt/JwtAuthenticationToken.java
+++ src/main/java/com/mumuk/global/security/jwt/JwtAuthenticationToken.java
@@ -1,5 +1,34 @@
 package com.mumuk.global.security.jwt;
 
-public class JwtAuthenticationToken {
-    // TODO: JWT 인증 토큰
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
+
+/**
+ *  액세스 토큰 검증 및 인증 여부 판단
+ */
+public class JwtAuthenticationToken extends AbstractAuthenticationToken {
+
+    private final Long user_id;
+    private final String token;
+
+
+    // 인증 후(인증 완료) 토큰용 생성자
+    public JwtAuthenticationToken(Long user_id, Collection<? extends GrantedAuthority> authorities) {
+        super(authorities);
+        this.token = null;
+        this.user_id = user_id;
+        super.setAuthenticated(true);
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return user_id;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return token;
+    }
 }
