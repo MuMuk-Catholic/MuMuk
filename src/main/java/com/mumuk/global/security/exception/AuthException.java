ef6d9a9af23a668be6c673134da2d7247ece630b
jaemin0413 <95339052+jaemin0413@users.noreply.github.com>
2026-04-13 00:57:15 +0900

    [FEAT] 인증인가 예외 처리

--- src/main/java/com/mumuk/global/security/exception/AuthException.java
+++ src/main/java/com/mumuk/global/security/exception/AuthException.java
@@ -1,5 +1,19 @@
 package com.mumuk.global.security.exception;
 
+import com.mumuk.global.apiPayload.code.ErrorCode;
+import com.mumuk.global.apiPayload.response.Response;
+import lombok.Getter;
+
+@Getter
 public class AuthException extends RuntimeException {
-    // TODO: 인증 예외 처리
+    private final ErrorCode errorCode;
+
+    public AuthException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public Response<Void> toResponse() {
+        return  Response.fail(errorCode);
+    }
 }
