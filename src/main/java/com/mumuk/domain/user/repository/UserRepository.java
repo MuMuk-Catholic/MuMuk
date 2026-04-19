eaba615ad6c13e8a8a060d56bda9602e64e0660f
jaemin0413 <95339052+jaemin0413@users.noreply.github.com>
2026-04-19 17:43:52 +0900

    [FEAT] 누락된 파일 추가

---
+++ src/main/java/com/mumuk/domain/user/repository/UserRepository.java
@@ -0,0 +1,23 @@
+package com.mumuk.domain.user.repository;
+
+import com.mumuk.domain.user.entity.User;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.mumuk.domain.user.entity.UserRecipe;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByLoginId(String loginId);
+    boolean existsByPhoneNumber(String phoneNumber);
+    boolean existsByNickName(String nickname);
+    Optional<User> findByLoginId(String loginId);
+    Optional<User> findByPhoneNumber(String phoneNumber);
+    Optional<User> findByEmail(String email);
+
+
+
+
+}
